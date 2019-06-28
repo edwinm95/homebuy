0d92a448fefaf13e7ebd4cf1c0938ff8842dbc71
Edwin Mwaniki <edwinmwaniki@Edwins-MBP.home>
2019-06-27 20:10:00 -0400

    Modified package.json file

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import configureStore from './store/index'
 import App from './components/App'
+import '@fortawesome/fontawesome-pro'
 import '@fortawesome/fontawesome-pro/css/all.css'
 import './stylesheet/main.css'
 
